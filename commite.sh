f235096bedbb51d172ed6bf0ebea1492f555a8d3
iassenegal221 <iassenegal221@gmail.com>
2022-01-28 23:33:22 +0100

    commit_msg

---
+++ commite.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config --global user.email "iassenegal221@gmail.com"
+git config --global user.name "iassenegal221"
+echo " commit message ?"
+read commit_msgs
+
+git add .
+git commit -m commit_msg
+git pull --rebase
+git push --set-upstream https://iassenegal221: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@github.com/iassenegal221/Newsletter.git master
